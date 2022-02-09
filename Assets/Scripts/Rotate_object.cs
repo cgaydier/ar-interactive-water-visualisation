346da88845f84278b69e68a3867adbfed3b1b1f1
Zoe Dty <zoe2801@gmail.com>
2022-02-09 15:39:52 +0100

    try things to move cube

--- Assets/Scripts/Rotate_object.cs
+++ Assets/Scripts/Rotate_object.cs
@@ -1,48 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.XR.ARFoundation;
-using UnityEngine.XR.ARSubsystems;
 
 
 public class Rotate_object : MonoBehaviour
 {
-    public GameObject objectToRotate;
-
     private float rotateSpeed = 0.09f;
 
-    private float cpt = 0f; //minute touched
-    private Vector2 firstPoint;
-    private Vector2 secondPoint;
-
-    // Update is called once per frame
     void Update()
     {
-        if (objectToRotate.activeSelf)
+        if (Input.touchCount > 0)
         {
-            if (Input.touchCount > 0)
-            {
-                Touch touch = Input.GetTouch(0);
-                if (touch.phase == TouchPhase.Began)
-                {
-                    firstPoint = touch.position;
-                }
-                else if (touch.phase == TouchPhase.Moved)
-                {
-                    if (touch.deltaPosition.x > 10f)
-                    {
-                        transform.rotation = Quaternion.Euler(0f, -1 * rotateSpeed, 0f) * transform.rotation;
-                    }
-
-                    else if (touch.deltaPosition.x < 10f)
-                    {
-                        transform.rotation = Quaternion.Euler(0f, 1 * rotateSpeed, 0f) * transform.rotation;
-                    }
-                }
-
-                return;
-            }
+            Debug.Log("La");
         }
-    }
+        float rotX = Input.GetAxis("Mouse X") * rotateSpeed * Mathf.Deg2Rad;
+        float rotY = Input.GetAxis("Mouse Y") * rotateSpeed * Mathf.Deg2Rad;
 
+        transform.Rotate(Vector3.up, -rotX);
+        transform.Rotate(Vector3.right, rotY);
+    }
 }
