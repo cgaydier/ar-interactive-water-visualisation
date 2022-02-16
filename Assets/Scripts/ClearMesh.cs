972b8a66047da27631c033257df474c81a3f4081
Lucas Miremont <lucas.miremont@gmail.com>
2022-02-16 14:16:01 +0100

    add clear points to ClearMesh

--- Assets/Scripts/ClearMesh.cs
+++ Assets/Scripts/ClearMesh.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class ClearMesh : MonoBehaviour
 {
+    public PlacePoints placePoints;
     void ClearM()
     {
         Destroy(GameObject.Find("Mesh"));
+        for(int i = 0; i < placePoints.nb_vertices; i++)
+            {
+                Destroy(placePoints.points[i]);
+            }
     }
 }
