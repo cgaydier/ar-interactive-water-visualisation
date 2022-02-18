608d7bf56879a418a101402fff3de3a6827c52e7
Zoe Dty <zoe2801@gmail.com>
2022-02-18 10:37:18 +0100

    fix points when open slider

--- Assets/Scripts/PlacePoints.cs
+++ Assets/Scripts/PlacePoints.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class PlacePoints : MonoBehaviour
 {
@@ -14,31 +15,33 @@ public class PlacePoints : MonoBehaviour
     public EnumState enumState;
     public List<GameObject> points = new List<GameObject>();
     public List<Vector3> vertices = new List<Vector3>();
+    public GraphicRaycaster GR;
     float offset = 0.2f;
 
-    //private void Start()
-    //{
-    //    Quaternion rot = Quaternion.identity;
-    //    points.Add(Instantiate(m_PointToPlace, new Vector3(0, 0, 0), rot));
-    //    points.Add(Instantiate(m_PointToPlace, new Vector3(1, 0, 0), rot));
-    //    points.Add(Instantiate(m_PointToPlace, new Vector3(1, 0, 1), rot));
-    //    points.Add(Instantiate(m_PointToPlace, new Vector3(0, 0, 1), rot));
-    //    points.Add(Instantiate(m_PointToPlace, new Vector3(0, 0, 0), rot));
-    //    points.Add(Instantiate(m_PointToPlace, new Vector3(1, 0, 0), rot));
-    //    points.Add(Instantiate(m_PointToPlace, new Vector3(1, 0, 1), rot));
-    //    points.Add(Instantiate(m_PointToPlace, new Vector3(0, 0, 1), rot));
+    private void Start()
+    {
+        GR = GameObject.Find("Canvas").GetComponent<GraphicRaycaster>();
+        //Quaternion rot = Quaternion.identity;
+        //points.Add(Instantiate(m_PointToPlace, new Vector3(0, 0, 0), rot));
+        //points.Add(Instantiate(m_PointToPlace, new Vector3(1, 0, 0), rot));
+        //points.Add(Instantiate(m_PointToPlace, new Vector3(1, 0, 1), rot));
+        //points.Add(Instantiate(m_PointToPlace, new Vector3(0, 0, 1), rot));
+        //points.Add(Instantiate(m_PointToPlace, new Vector3(0, 0, 0), rot));
+        //points.Add(Instantiate(m_PointToPlace, new Vector3(1, 0, 0), rot));
+        //points.Add(Instantiate(m_PointToPlace, new Vector3(1, 0, 1), rot));
+        //points.Add(Instantiate(m_PointToPlace, new Vector3(0, 0, 1), rot));
 
-    //    vertices.Add(new Vector3(0, 0, 0));
-    //    vertices.Add(new Vector3(0, 0 + offset, 0));
-    //    vertices.Add(new Vector3(1, 0, 0));
-    //    vertices.Add(new Vector3(1, 0 + offset, 0));
-    //    vertices.Add(new Vector3(1, 0, 1));
-    //    vertices.Add(new Vector3(1, 0 + offset, 1));
-    //    vertices.Add(new Vector3(0, 0, 1));
-    //    vertices.Add(new Vector3(0, 0 + offset, 1));
+        //vertices.Add(new Vector3(0, 0, 0));
+        //vertices.Add(new Vector3(0, 0 + offset, 0));
+        //vertices.Add(new Vector3(1, 0, 0));
+        //vertices.Add(new Vector3(1, 0 + offset, 0));
+        //vertices.Add(new Vector3(1, 0, 1));
+        //vertices.Add(new Vector3(1, 0 + offset, 1));
+        //vertices.Add(new Vector3(0, 0, 1));
+        //vertices.Add(new Vector3(0, 0 + offset, 1));
 
-    //    nb_vertices = 8;
-    //}
+        //nb_vertices = 8;
+    }
 
     public void clearAll()
     {
@@ -61,14 +64,25 @@ public class PlacePoints : MonoBehaviour
             
             if(touch.phase == TouchPhase.Began)
             {
-                if(m_RaycastManager.Raycast(touch.position, s_Hits, TrackableType.PlaneWithinPolygon))
+                if (m_RaycastManager.Raycast(touch.position, s_Hits, TrackableType.PlaneWithinPolygon))
                 {
+                    PointerEventData ped = new PointerEventData(null)
+                    {
+                        position = touch.position
+                    };
+                    List<RaycastResult> results = new List<RaycastResult>();
+                    GR.Raycast(ped, results);
 
-                    Pose hitPose = s_Hits[0].pose;
-                
-                    points.Add(Instantiate(m_PointToPlace, hitPose.position, hitPose.rotation));
-                    vertices.Add(hitPose.position);
-                    vertices.Add(new Vector3(hitPose.position.x, hitPose.position.y + offset, hitPose.position.z));
+                    Debug.Log(results);
+                    print(results.Count);
+                    if (results.Count == 1)
+                    {
+                        Pose hitPose = s_Hits[0].pose;
+
+                        points.Add(Instantiate(m_PointToPlace, hitPose.position, hitPose.rotation));
+                        vertices.Add(hitPose.position);
+                        vertices.Add(new Vector3(hitPose.position.x, hitPose.position.y + offset, hitPose.position.z));
+                    }
                 }
             }
         }
