8be310261341a47a499e7a385dbb05571a8f8319
Lucas Miremont <lucas.miremont@gmail.com>
2022-02-21 10:52:19 +0100

    print dist for surface

--- Assets/Scripts/CreateMesh.cs
+++ Assets/Scripts/CreateMesh.cs
@@ -53,8 +53,9 @@ public class CreateMesh : MonoBehaviour
             triangles.Add(0);
             triangles.Add(j + 4);
             triangles.Add(j + 2);
-
-            surface = surface + ((Vector3.Distance(placePoints.vertices[0], placePoints.vertices[j+2] * Vector3.Distance(placePoints.vertices[j+2], placePoints.vertices[j+4]))) / 2);
+            print("longueur 0 et " + (j+2) + " : " + (Vector3.Distance(placePoints.vertices[0], placePoints.vertices[j+2])).ToString("F2"));
+            print("longueur " + (j+2) + " et " + (j+4) + " : " + (Vector3.Distance(placePoints.vertices[j+2], placePoints.vertices[j+4])).ToString("F2"));
+            surface = surface + ((Vector3.Distance(placePoints.vertices[0], placePoints.vertices[j+2]) * Vector3.Distance(placePoints.vertices[j+2], placePoints.vertices[j+4])) / 2);
 
             // Top
             triangles.Add(1);
@@ -89,7 +90,7 @@ public class CreateMesh : MonoBehaviour
         {
             mesh.vertices = placePoints.vertices.ToArray();
             // convert to meter
-            surface = CreateTriangles() * 0.833f;
+            surface = CreateTriangles();
             GameObject.Find("SurfaceM2").GetComponent<UnityEngine.UI.Text>().text = "Surface : " + surface.ToString("F2") + " m2";
             mesh.triangles = triangles.ToArray();
             mesh.MarkDynamic();
