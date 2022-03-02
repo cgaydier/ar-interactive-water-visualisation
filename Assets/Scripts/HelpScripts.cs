c0304c317f3377a77d3ec795032f02b130e87ce5
Antoine D <antoine.deynoux@outlook.fr>
2022-03-02 16:43:00 +0100

    Add 2 pages

--- Assets/Scripts/HelpScripts.cs
+++ Assets/Scripts/HelpScripts.cs
@@ -5,17 +5,28 @@ using UnityEngine;
 public class HelpScripts : MonoBehaviour
 {
     public GameObject UIPanel;
-    bool isActive = true;
+    public GameObject Page_1;
+    public GameObject Page_2;
+    public GameObject CurrentPage;
+
+    bool isPanelActive = true;
 
     public void OpenAndClosePanel()
     {
-        if (isActive == false) {
-            UIPanel.SetActive(!isActive);
-            isActive = true;
-        }else
-        {
-            UIPanel.SetActive(!isActive);
-            isActive = false;
+        if (isPanelActive == false) {
+            UIPanel.SetActive(!isPanelActive);
+            isPanelActive = true;
+        }else{
+            UIPanel.SetActive(!isPanelActive);
+            isPanelActive = false;
+        }
+    }
+
+    public void ChangePage()
+    {
+        if(CurrentPage == Page_1){
+            Page_1.SetActive(false);
+            Page_2.SetActive(true);
         }
     }
 }
