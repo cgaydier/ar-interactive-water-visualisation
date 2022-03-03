1eeebdf7950381bd777c3ea2c4b94d9e5f70f966
Antoine D <antoine.deynoux@outlook.fr>
2022-03-03 17:29:16 +0100

    UI Tutorial ready to work

--- Assets/Scripts/HelpScripts.cs
+++ Assets/Scripts/HelpScripts.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class HelpScripts : MonoBehaviour
 {
     public GameObject UIPanel;
-    public GameObject Page_1, Page_2, Page_3;
+    public GameObject Page_1, Page_2, Page_3, Page_4;
     public GameObject CurrentPage;
     public GameObject Menu;
     public GameObject PrevButton, NextButton;
@@ -32,15 +32,17 @@ public class HelpScripts : MonoBehaviour
 
     public void NextPage()
     {
-        if(CurrentPage == Page_1){
-            
+        if(CurrentPage == Page_1){  
             SwitchPage(Page_1, Page_2);
             PrevButton.SetActive(true);
         }
 
         else if(CurrentPage == Page_2){
             SwitchPage(Page_2, Page_3);
-            
+        }
+
+        else if(CurrentPage == Page_3){
+            SwitchPage(Page_3, Page_4);
             NextButton.SetActive(false);
         }
     }
@@ -49,16 +51,16 @@ public class HelpScripts : MonoBehaviour
     {
         if(CurrentPage == Page_2){
             SwitchPage(Page_2, Page_1);
-
-            
             PrevButton.SetActive(false);
         }
 
         else if(CurrentPage == Page_3){
-            NextButton.SetActive(true);
-
-            
             SwitchPage(Page_3, Page_2);
+        }
+
+        else if(CurrentPage == Page_4){
+            NextButton.SetActive(true);
+            SwitchPage(Page_4, Page_3);
         }
     }
 
