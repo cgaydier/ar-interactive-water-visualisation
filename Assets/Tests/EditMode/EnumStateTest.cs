f985feaaaa1e3516436091767d7b415256411805
Unknown <c.gaydier@laposte.net>
2022-03-14 09:49:35 +0100

    Push Enum State Unit Test prototype

--- Assets/Tests/EditMode/EnumStateTest.cs
+++ Assets/Tests/EditMode/EnumStateTest.cs
@@ -6,10 +6,81 @@ using UnityEngine.TestTools;
 
 public class EnumState
 {
-    // A Test behaves as an ordinary method
+    public enum State {MainView, ParamScene, PlacePoints, PlacePointsBefore, ConsumptionScene}
+      public State currentState;
+
     [Test]
-    public void EnumStateSimplePasses()
-    { 
-        // Use the Assert class to test conditions
+    public void SetParamScene_ShouldReturn()
+    {
+        //SetParamScene();
+        Assert.AreEqual(currentState, State.ParamScene);
     }
+
+    [Test]
+    public void SetMainScene_ShouldReturn()
+    { 
+        //SetMainScene();
+        Assert.AreEqual(currentState, State.MainView);
+
+    }
+
+    [Test]
+    public void SetPlacePoints_ShouldReturn()
+    { 
+        //SetPlacePoints();
+        Assert.AreEqual(currentState, State.PlacePoints);
+
+    }
+
+    [Test]
+    public void SetPlacePointsBefore_ShouldReturn()
+    { 
+        //SetPlacePointsBefore();
+        Assert.AreEqual(currentState, State.PlacePointsBefore);
+
+    }
+
+    [Test]
+    public void SetConsumptionScene_ShouldReturn()
+    { 
+        //SetConsumptionScene();
+        Assert.AreEqual(currentState, State.PlacePointsBefore);
+
+    }
+
+    public void ChangeParamSceneToParamScene()
+    {
+        //SetParamScene();
+        //ChangeParamScene();
+        Assert.AreEqual(currentState, State.ParamScene);
+    }
+
+    public void ChangeParamSceneToPlacePoints()
+    {
+        //SetMainScene();
+        //ChangeMainScene();
+        Assert.AreEqual(currentState, State.PlacePoints);
+    }
+
+    public void ChangeParamSceneToPlacePointsBefore()
+    {
+        //SetPlacePointsBefore();
+        //ChangeMainScene();
+        Assert.AreEqual(currentState, State.PlacePointsBefore);
+    }
+
+    public void ChangeParamSceneToMainScene()
+    {
+        //SetMainScene();
+        //ChangeMainScene();
+        Assert.AreEqual(currentState, State.MainView);
+    }
+
+    public void ChangeModalSceneToMainScene()
+    {
+        //SetConsumptionScene();
+        //ChangeModalScene();
+        Assert.AreEqual(currentState == State.ConsumptionScene, "Modal ConsumptionScene is not load correctly");
+    }
+
 }
