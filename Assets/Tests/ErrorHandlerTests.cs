e5576beb76283cc70bc1d8bca0bb4db5373b73d2
Lucas Miremont <lucas.miremont@gmail.com>
2022-03-21 11:16:59 +0100

    add errorHandler tests

--- Assets/Tests/ErrorHandlerTests.cs
+++ Assets/Tests/ErrorHandlerTests.cs
@@ -1,25 +1,79 @@
-using System.Collections;
-using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
-using UnityEngine.TestTools;
+
 
 public class ErrorHandlerTests
 {
-    // A Test behaves as an ordinary method
-    [Test]
-    public void ErrorHandlerTestsSimplePasses()
+    private ErrorHandler errorHandler;
+    private string debugMessage;
+    
+    private void StartFunction()
     {
-        // Use the Assert class to test conditions
+        errorHandler = GameObject.Find("ErrorHandler").GetComponent<ErrorHandler>();
+        debugMessage = errorHandler.GetDebugMessage();
     }
 
-    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-    // `yield return null;` to skip a frame.
-    [UnityTest]
-    public IEnumerator ErrorHandlerTestsWithEnumeratorPasses()
+    // A Test behaves as an ordinary method
+    [Test]
+    public void PlacePointsErrorTest()
     {
-        // Use the Assert class to test conditions.
-        // Use yield to skip a frame.
-        yield return null;
+        StartFunction();
+        errorHandler.PlacePointsError();
+        debugMessage = errorHandler.GetDebugMessage();
+        Assert.AreEqual(debugMessage, "Max points reached (10).\nPlease validate or clear.\n");
+    }
+
+    [Test]
+    public void AlreadyCreatedErrorTest()
+    {
+        StartFunction();
+        errorHandler.AlreadyCreatedError();
+        debugMessage = errorHandler.GetDebugMessage();
+        Assert.AreEqual(debugMessage, "Points already placed.\nPlease validate or clear.\n");
+    }
+
+    [Test]
+    public void AlreadyValidatedErrorTest()
+    {
+        StartFunction();
+        errorHandler.AlreadyValidatedError();
+        debugMessage = errorHandler.GetDebugMessage();
+        Assert.AreEqual(debugMessage, "Surface already validated.\n");
+    }
+
+    [Test]
+    public void AlreadyClearedErrorTest()
+    {
+        StartFunction();
+        errorHandler.AlreadyClearedError();
+        debugMessage = errorHandler.GetDebugMessage();
+        Assert.AreEqual(debugMessage, "Nothing to clear.\n");
+    }
+
+    [Test]
+    public void NoPointsErrorTest()
+    {
+        StartFunction();
+        errorHandler.NoPointsError();
+        debugMessage = errorHandler.GetDebugMessage();
+        Assert.AreEqual(debugMessage, "Not enough points.\nAt least 3 are needed.\n");
+    }
+
+    [Test]
+    public void NoVolumeErrorTest()
+    {
+        StartFunction();
+        errorHandler.NoVolumeError();
+        debugMessage = errorHandler.GetDebugMessage();
+        Assert.AreEqual(debugMessage, "Please create a water volume.\n");
+    }
+
+    [Test]
+    public void ErrorMessageReset()
+    {
+        StartFunction();
+        errorHandler.ErrorMessageReset();
+        debugMessage = errorHandler.GetDebugMessage();
+        Assert.AreEqual(debugMessage, "");
     }
 }
