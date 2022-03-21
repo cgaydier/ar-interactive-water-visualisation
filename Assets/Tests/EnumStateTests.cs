e3c158cbdc86da29e889837e39d5c2dceb48ae93
Zoe Dty <zoe2801@gmail.com>
2022-03-21 11:04:56 +0100

    enumState tests finish

--- Assets/Tests/EnumStateTests.cs
+++ Assets/Tests/EnumStateTests.cs
@@ -6,20 +6,74 @@ using UnityEngine.TestTools;
 
 public class EnumStateTests
 {
-    // A Test behaves as an ordinary method
-    [Test]
-    public void EnumStateTestsSimplePasses()
+    private EnumState enumState;
+
+    private void StartFunction()
     {
-        // Use the Assert class to test conditions
+        enumState = GameObject.Find("SceneState").GetComponent<EnumState>();
     }
 
-    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-    // `yield return null;` to skip a frame.
-    [UnityTest]
-    public IEnumerator EnumStateTestsWithEnumeratorPasses()
+    // A Test behaves as an ordinary method
+    [Test]
+    public void GetStateTest()
     {
-        // Use the Assert class to test conditions.
-        // Use yield to skip a frame.
-        yield return null;
+        StartFunction();
+        enumState.currentState = EnumState.State.MainView;
+        Assert.AreEqual(enumState.GetState(), EnumState.State.MainView);
+    }
+
+    [Test]
+    public void ChangeSettingSceneTest()
+    {
+        StartFunction();
+        enumState.currentState = EnumState.State.MainView;
+        enumState.ChangeSettingScene();
+
+        Assert.AreEqual(enumState.GetState(), EnumState.State.ConsumptionScene);
+
+        enumState.currentState = EnumState.State.PlacePoints;
+        enumState.ChangeSettingScene();
+
+        Assert.AreEqual(enumState.GetState(), EnumState.State.ConsumptionScene);
+
+        enumState.ChangeSettingScene();
+
+        Assert.AreEqual(enumState.GetState(), EnumState.State.MainView);
+    }
+
+    [Test]
+    public void SetMainSceneTest()
+    {
+        StartFunction();
+        enumState.currentState = EnumState.State.PlacePoints;
+        enumState.SetMainScene();
+        Assert.AreEqual(enumState.GetState(), EnumState.State.MainView);
+    }
+
+    [Test]
+    public void SetPlacePointsTest()
+    {
+        StartFunction();
+        enumState.currentState = EnumState.State.MainView;
+        enumState.SetPlacePoints();
+        Assert.AreEqual(enumState.GetState(), EnumState.State.PlacePoints);
+    }
+
+    [Test]
+    public void SetConsumptionSceneTest()
+    {
+        StartFunction();
+        enumState.currentState = EnumState.State.MainView;
+        enumState.SetConsumptionScene();
+        Assert.AreEqual(enumState.GetState(), EnumState.State.ConsumptionScene);
+    }
+
+    [Test]
+    public void SetTutoTest()
+    {
+        StartFunction();
+        enumState.currentState = EnumState.State.MainView;
+        enumState.SetTuto();
+        Assert.AreEqual(enumState.GetState(), EnumState.State.Tuto);
     }
 }
