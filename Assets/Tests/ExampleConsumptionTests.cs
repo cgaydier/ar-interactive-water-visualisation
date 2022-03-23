584cf8667326e5891b8de8b87978fac386b5cd99
Lucas Miremont <lucas.miremont@gmail.com>
2022-03-23 12:37:08 +0100

    add ExampleConsumption test

--- Assets/Tests/ExampleConsumptionTests.cs
+++ Assets/Tests/ExampleConsumptionTests.cs
@@ -1,9 +1,32 @@
 using NUnit.Framework;
+using UnityEngine;
 
 public class ExampleConsumptionTests
 {
+    private SceneData sceneData;
+    private GameObject examplePanel;
+    private ExampleConsumption exampleConsumption;
+
+    private void StartFunction()
+    {
+        sceneData = GameObject.Find("SceneData").GetComponent<SceneData>();
+        examplePanel = GameObject.Find("Content").GetComponent<ScrollButtonFunctions>().examplePanel;
+        examplePanel.SetActive(true);
+        exampleConsumption = GameObject.Find("AverageConso").GetComponent<ExampleConsumption>();
+        exampleConsumption.Start();
+    }
+
     [Test]
     public void SetValueTest()
     {
+        StartFunction();
+
+        string whichOne;
+
+        whichOne = "Average1P1W";
+        exampleConsumption.SetValue(whichOne);
+
+        Assert.IsFalse(examplePanel.activeSelf);
+        examplePanel.SetActive(false);
     }
 }
